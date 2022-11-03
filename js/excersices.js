e7489f269e795d74d885feb4f0a1440d4a0b5363
Nahomy <117209373+Jayjaybin@users.noreply.github.com>
2022-11-03 00:23:37 -0600

    Escribí en el script

--- js/excersices.js
+++ js/excersices.js
@@ -5,3 +5,23 @@ function Sumar(){
     document.getElementById("txtResultado").innerHTML = 'Resultado: ' + resultado;
 }
 
+function CalcularOperacion(){
+    let op =document.getElementById('selectOp').value;
+    let num1 =parseInt(document.getElementById('txt2Num1').value);
+    let num2 =parseInt(document.getElementById('txt2Num2').value);
+    if(op ==0){
+        alert("Seleccione primero la operación");
+    }
+    else if(op ==1){
+        document.getElementById('txt2Result').innerHTML="La suma es: "+(num1 +num2);
+    }
+    else if(op ==2){
+        document.getElementById('txt2Result').innerHTML="La resta es: "+(num1 -num2);
+    }
+    else if(op ==3){
+        document.getElementById('txt2Result').innerHTML="El producto es: "+(num1 *num2);
+    }
+    else if(op ==4){
+        document.getElementById('txt2Result').innerHTML="La division es: "+(num1 /num2);
+    }
+}
